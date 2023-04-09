591ef49213e2660693131f3bf7c0b3ddccf37871
RobertAngerer <37867984+RobertAngerer@users.noreply.github.com>
2023-04-09 19:58:36 +0200

    map <c-n> and <c-p> do down and up

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -14,3 +14,12 @@ hs.hotkey.bind({"left_command", "left_option", "left_shift", "left_control"}, "A
 hs.hotkey.bind({"left_command", "left_option", "left_shift", "left_control"}, "D", open_app("Alacritty"))
 hs.hotkey.bind({"left_command", "left_option", "left_shift", "left_control"}, "F", open_app("Microsoft Teams"))
 hs.hotkey.bind({"left_command", "left_option", "left_shift", "left_control"}, "K", open_app("Emacs"))
+
+local function keyCode(key, modifiers)
+    return function()
+        hs.eventtap.keyStroke(modifiers, key, 1000)
+    end
+end
+
+hs.hotkey.bind({"ctrl"}, "n", keyCode("down", {}), nil, keyCode("down", {}))
+hs.hotkey.bind({"ctrl"}, "p", keyCode("up", {}), nil, keyCode("up", {}))
